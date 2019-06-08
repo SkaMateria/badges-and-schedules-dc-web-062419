a9e066cb3243b74bd0e9f0df00f7844e4a0e8108
Carlos Velazquez <skamateria@gmail.com>
2019-06-08 12:53:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each { |n| return "Hello, my name is #{n}."}
+end
